ef28732bec30ab92bebcd9fe2d5a630a792560fb
Michael Goldstein <mlgoldstein28@gmail.com>
2023-05-25 15:55:41 -0400

    Initialization. Homepage. Navbar.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import styles from './App.module.scss';
+import bgImage from './media/IMG_0236.JPG';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={styles.myContainer}>
+        <Navbar />
+        <div className={styles.myTextContainer}>
+          <h1 className={styles.myName}>Michael Goldstein</h1>
+          <p className={styles.myIntro}>I'm a <span className="fw-bold">Frontend Developer</span><br/>specializing in <span className="text-decoration-underline">ReactJS</span> and determined<br/>to deliver seamless products.</p>
+          <button className="btn btn-outline-light fs-3 mt-3">My Projects</button>
+      </div> 
+      </div>
+      <div className={styles.imgContainer}>
+          <img src={bgImage} className={styles.img} alt="backgroundImage" />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import gitHubLogo from '../../media/github-mark.png';
+import styles from './Navbar.module.scss'
+export default function Navbar() {
+    return (
+        <div className="Navbar">
+            <nav className="">
+                <a className="" href="https://github.com/mlgoldstein28">
+                    <img src={gitHubLogo} className={styles.logo}/>
+                </a>
+                <div className="navbar-nav">
+                  <ul className="d-flex">
+                    <li className={`nav-item nav-link ${styles.tabs}`} href="/">Projects</li>
+                    <li className={`nav-item nav-link ${styles.tabs}`} href="/">About Me</li>
+                    <li className={`nav-item nav-link ${styles.tabs}`} href="/">Contact</li>
+                  </ul>
+                </div>
+            </nav>
+        </div>
+    )
+}
