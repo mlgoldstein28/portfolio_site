f0d87520abcda3af0be29bdda6a6b2e78bd980a0
Michael Goldstein <mlgoldstein28@gmail.com>
2023-05-26 13:37:07 -0400

    "Projects" page creation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 import styles from './App.module.scss';
 import bgImage from './media/IMG_0236.JPG';
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
@@ -12,13 +13,14 @@ function App() {
         <Navbar />
         <div className={styles.myTextContainer}>
           <h1 className={styles.myName}>Michael Goldstein</h1>
-          <p className={styles.myIntro}>I'm a <span className="fw-bold">Frontend Developer</span><br/>specializing in <span className="text-decoration-underline">ReactJS</span> and determined<br/>to deliver seamless products.</p>
+          <p className={styles.myIntro}><span className="fw-bold">Frontend Developer</span><br/>Specializing in <span className="text-decoration-underline">ReactJS</span> and determined<br/>to deliver seamless products.</p>
           <button className="btn btn-outline-light fs-3 mt-3">My Projects</button>
       </div> 
       </div>
       <div className={styles.imgContainer}>
           <img src={bgImage} className={styles.img} alt="backgroundImage" />
       </div>
+      <Projects />
     </div>
   );
 }
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,21 @@
+
+const Projects = () => {
+    return (
+        <div>
+            <div className="w-100 vh-100 bg-dark">
+               <h1 className="text-light pt-5 text-center" style={{fontSize: "80px"}}>Projects</h1>
+               <hr className="text-light w-50 m-auto"/>
+               <div className="container row m-auto mt-4">
+                    <div className="col-lg-3 col-12 m-auto border border-primary">
+                        <img />
+                    </div>
+                    <div className="col-lg-3 col-12 m-auto border border-primary">
+
+                    </div>
+               </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects;
