3e2d2e093656ca7913fd09af876e2707cdcacfa7
Michael Goldstein <mlgoldstein28@gmail.com>
2023-05-30 12:20:24 -0400

    Projects UX/UI

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,18 +2,35 @@ import {useState} from 'react'
 import chipThumb from '../../media/chipThumb.png';
 import styles from './Projects.module.scss';
 import { Link } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar'
 
 const Projects = () => {
-
     return (
         <div>
-            <div className="w-100 vh-100 bg-dark">
-               <h1 className="text-light pt-5 text-center" style={{fontSize: "80px"}}>Projects</h1>
-               <hr className="text-light w-50 m-auto"/>
+            <div className={styles.container}>
+            <Navbar />
+               <h1 className="text-center text-light fw-bold w-25 m-auto mb-2" style={{fontSize: "80px"}}>Portfolio</h1>
+               <hr className=" w-50 m-auto"/>
                <div className="container row mt-4 d-flex m-auto">
                     <div className={`${styles.projectContainer} m-auto text-light text-center`}>
-                        <h2>Chipotle Clone</h2>
-                        <Link to="/chip"><img className={styles.img} src={chipThumb} alt="Chipotle-Clone" /></Link>
+                        <h2 className="mt-3 fw-bold">Chipotle Clone</h2>
+                        <Link to="/chip"><img className={`${styles.img} shadow-lg`} src={chipThumb} alt="Chipotle-Clone" /></Link>
+                        <p className="mt-3 fw-bold fs-5 mb-0">Key Skills/Tools: </p>
+                        <p className="fw-bold">ReactJS, JavaScript, Bootstrap, SASS, GitHub</p>
+                        <p className={`${styles.description}`}>In this project, I focused on design and attention to detail.</p>
+                    </div>
+                    <div className={`${styles.projectContainer} m-auto  text-center`}>
+                        <h2 className="mt-3 fw-bold">Chipotle Clone</h2>
+                        <Link to="/chip"><img className={`${styles.img} shadow-lg`} src={chipThumb} alt="Chipotle-Clone" /></Link>
+                        <p className="mt-3 fw-bold fs-5 mb-0">Key Skills/Tools: </p>
+                        <p className="fw-bold">ReactJS, JavaScript, Bootstrap, SASS, GitHub</p>
+                        <p className={`${styles.description}`}>In this project, I focused on design and attention to detail.</p>
+                    </div>
+                    <div className={`${styles.projectContainer} m-auto  text-center`}>
+                        <h2 className="mt-3 fw-bold">Chipotle Clone</h2>
+                        <Link to="/chip"><img className={`${styles.img} shadow-lg`} src={chipThumb} alt="Chipotle-Clone" /></Link>
+                        <p className="mt-3 fw-bold fs-5 mb-0">Key Skills/Tools: </p>
+                        <p className="fw-bold">ReactJS, JavaScript, Bootstrap, SASS, GitHub</p>
                         <p className={`${styles.description}`}>In this project, I focused on design and attention to detail.</p>
                     </div>
                     <div className="col-lg-3 col-12 m-auto">
