6140f95c3dc1af2ef09c91548c7cbbc7e0e9db84
Michael Goldstein <mlgoldstein28@gmail.com>
2023-06-09 16:37:42 -0400

    Resume update

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -10,13 +10,15 @@ const ProjectCards = ({projectDisplay, projects}) => {
             <div className={`${styles.projectContainer} m-auto text-light`}>
                 <h2 className="mt-3 mb-3 text-center fw-bold">{title}</h2>
                 <hr className="w-50 m-auto mb-3"/>
-                <a href={demoLink}><div className={styles.imgContainer}>
-                    <img className={`${styles.img} shadow-lg shadow-white`} src={image} alt={title} />
+                <a href={demoLink}>
+                  <div className={styles.imgContainer}>
+                        <img className={`${styles.img} shadow-lg shadow-white`} src={image} alt={title} />
                         <div className={styles.summary}>{summaryOne} <br/><br/>
                             <a href={demoLink} className="text-decoration-underline m-2">Live Demo Here</a>
                             <a href={gitHubLink} className="text-decoration-underline m-2">Code Here</a>
                         </div>
-                </div></a>
+                  </div>
+                </a>
             </div>
         </div>
     )
