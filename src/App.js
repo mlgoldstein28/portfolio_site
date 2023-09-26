a585697ed7cedb670eb49bfa93a41a7a538b9886
Michael Goldstein <mlgoldstein28@gmail.com>
2023-09-26 18:19:03 -0400

    Title update

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ export default function App() {
       <div className={styles.myContainer}>
       <Navbar />
         <div className={styles.myTextContainer}>
-          <h1 className={styles.myTitle}>Frontend Developer</h1>
+          <h1 className={styles.myTitle}>Software Developer</h1>
           <p className={`${styles.myIntro}`}><span className="text-decoration-underline">ReactJS</span> Specialist <br/> Determined to deliver seamless products</p>
           <a href="#projects"><button className="btn btn-outline-light fs-3 m-3">My Work</button></a>
           <a href="#contact"><button className="btn btn-outline-light fs-3 m-3">My Information</button></a>
             <p className={styles.myStack}>
-                <span><img src={jsIcon} alt="JS" className={styles.stackLogo}/></span>
+                <span><img src={jsIcon} alt="" className={styles.stackLogo}/></span>
                 <span><img src={reactIcon} alt="" className={styles.stackLogo}/></span>
                 <span><img src={nodeJSIcon} alt="" className={styles.stackLogo}/></span>
                 <span><img src={htmlIcon} alt="" className={styles.stackLogo}/></span>
